2c984e385719b4ee9121fb20c1bf3cad99314f89
8BitJonny <timon99@gmx.de>
2020-02-08 21:49:16 +0100

    first layout

---
+++ components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label for="search" class="label">Search</label>
+    <input id="search" class="input" ref="input" type="text" placeholder='Press "/" to focus'>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  mounted() {
+    window.addEventListener('keyup', (event) => {
+      if (event.key === "/") {
+        this.$refs.input.focus();
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .label {
+    @apply text-xl font-semibold mr-2
+  }
+  .input {
+    @apply rounded-md py-1 px-2 text-black
+  }
+</style>
