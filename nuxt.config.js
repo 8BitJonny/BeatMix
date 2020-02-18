b861cfd29dab31ac377ad16e604936663c13c015
8BitJonny <timon99@gmx.de>
2020-02-18 23:08:50 +0100

    fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Fusify - Artist Mixer',
+    title: 'BeatMix - Artist Mixer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
